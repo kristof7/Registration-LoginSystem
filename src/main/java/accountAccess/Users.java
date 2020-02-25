98d83db4df5383d11392cc5ce55012929d955023
Krzysztof Kampa <krzysztofk7777@gmail.com>
2020-02-25 18:17:17 +0100

    Registration system update, add Json

---
+++ src/main/java/accountAccess/Users.java
@@ -0,0 +1,24 @@
+package accountAccess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+
+    private List<User> users = new ArrayList<>();
+
+    public Users() {
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public void add(User user) {
+        users.add(user);
+    }
+}
